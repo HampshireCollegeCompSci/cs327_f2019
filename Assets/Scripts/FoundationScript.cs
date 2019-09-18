d4b490c1e844e40a4d83c4d706684a64e5148bb9
Max <magicmaxmark@gmail.com>
2019-09-17 22:49:52 -0400

    Added Foundation Script

---
+++ Assets/Scripts/FoundationScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoundationScript : MonoBehaviour
+{
+    public List<GameObject> cardList;
+    public bool addCard;
+    public bool removeCard;
+    public GameObject cardToAdd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (removeCard == true)
+        {
+            cardList.Remove(cardList[0]);
+            removeCard = false;
+        }
+
+        if (addCard == true)
+        {
+            cardList.Insert(0, cardToAdd);
+            addCard = false;
+        }
+    }
+}
