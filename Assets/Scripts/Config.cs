5793b5e4f954832772e047b7ca1919b7489cf3dc
Per Van Dyke <pav17@hampshire.edu>
2019-09-20 13:53:50 -0400

    Initial version of Config

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour
+{
+    public static Config config; //Creates a new instance if one does not yet exist
+
+    //Variables go here
+
+
+
+
+    void Awake()
+    {
+        if (config == null)
+        {
+            DontDestroyOnLoad(gameObject); //makes instance persist across scenes
+            config = this;
+        }
+        else if (config != this)
+        {
+            Destroy(gameObject); //deletes copies of global which do not need to exist, so right version is used to get info from
+        }
+    }
+}
