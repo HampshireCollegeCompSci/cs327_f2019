3822c405e28117ae1b64a32ed8e6885368313505
Max <magicmaxmark@gmail.com>
2019-09-19 20:29:26 -0400

    Finalised Code Added Comments
    Added as many methods as I could

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -13,27 +13,38 @@ public class CardScript : MonoBehaviour
     public string cardNum;
     //cardNum is the number on the card, ace is 1 jack is 11 queen is 12 king is 13
     public bool isSelected;
-   
-    void Update()
+
+    void SetCardAppearance()
     {
+        //shows card back if it's hidden
         if (hidden == true)
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = cardBackSprite;
         }
 
+        //shows card if it's not hidden
         if (hidden == false)
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = cardSprite;
         }
 
+        //makes card larger and first in sorting order if the card is selected
         if (isSelected == true)
         {
             gameObject.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+            gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "SelectedCard";
         }
 
+        //makes card normal if not selected
         if (isSelected == false)
         {
             gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Default";
         }
+    }
+   
+    void Update()
+    {
+        SetCardAppearance();
     }
 }
--- Assets/Scripts/FoundationScript.cs
+++ Assets/Scripts/FoundationScript.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class FoundationScript : MonoBehaviour
 {
     public List<GameObject> cardList;
-    public GameObject cardToAdd;
     int counter;
     int cardMax;
-    int locationInList;
 
+    //adds a card to the list of cards on the foundation
     public void AddCard(GameObject cardToAdd, int locationInList)
     {
         cardList.Insert(locationInList, cardToAdd);
     }
 
+    //removes a card from the list of cards on the foundation
     public void RemoveCard(int locationInList)
     {
         if (cardList[locationInList].gameObject.transform.parent = gameObject.transform)
@@ -25,17 +25,24 @@ public class FoundationScript : MonoBehaviour
         cardList.Remove(cardList[locationInList]);
     }
 
-    void Update()
+    //assigns card positions and render order and sets this foundation as the cards parents
+    void SetCardPositions()
     {
         counter = 0;
         cardMax = cardList.Count;
 
-        while(counter < cardMax)
+        while (counter < cardMax)
         {
             cardList[counter].transform.parent = gameObject.transform;
             cardList[counter].transform.localPosition = new Vector3(0, -0.5f * counter, 0);
             cardList[counter].gameObject.GetComponent<SpriteRenderer>().sortingOrder = cardMax - counter;
             counter += 1;
         }
+    }
+
+   
+    void Update()
+    {
+        SetCardPositions();
     }
 }
