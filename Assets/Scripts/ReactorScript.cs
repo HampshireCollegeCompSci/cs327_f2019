d7c8ef1c54a99d50b799aa1dd7f41cac9c982c16
sophiaberger <sophia.alexandra.berger@gmail.com>
2019-09-22 15:07:06 -0400

    Created MatchSuit method
    Simply a boolean for checking if the suit matches

--- Assets/Scripts/ReactorScript.cs
+++ Assets/Scripts/ReactorScript.cs
@@ -6,13 +6,22 @@ public class ReactorScript : MonoBehaviour
 {
     //helloWorld
     public List<GameObject> cardList;
+    public UtilsScript utils;
     int counter;
     int cardMax;
     int ReactorVal;
 
+    
+    
     void Update()
     {
-        
+        //constantly checking to see if reactor score is below
+        if (CountReactorCard() >= 18)
+        {
+            //TODO: game over
+            
+
+        }
     }
 
     public void SetCardPositions()
@@ -29,30 +38,64 @@ public class ReactorScript : MonoBehaviour
         }
     }
 
+    //this function is getting called when clicked
     public void Clicked()
     {
-        if (CountReactorCard() < 18)
+
+        //change later, just using so the code block isn't ugly
+        //if the card numbers match
+        if (utils.selectedCards[0].GetComponent<CardScript>().cardNum == utils.clcikedCard.GetComponent<CardScript>().cardNum)
         {
-            //TODO:
+            if (MatchSuit())
+            {
+                utils.Match();
+            }
+        }
             
-        }
-        else
-        {
-            //TODO: game over
-        }
+            //this will work once Max updates it to be public
+        
+        
         return;
-        //on click begin reactor function
-        //from utils
-        //if the count is under 18
-            //when you call Count Reactor Card
-            //check if a match
-            //if it's a match then do something? Unclear how to do
-            //else don't need to do anything
-        //else
-            //game over
+        //if the cards match (check the attributes of the cards - you're only allowed to click top card
+        //not sure how to compare to other card yet
+        //then call match function
+        
 
     }
 
+    public bool MatchSuit()
+    {
+        //just to make it cleaner because this utils.blah blah blah is yucky
+        //basically a string of if/else cases for matching
+        string selectedCardSuit = utils.selectedCards[0].GetComponent<CardScript>().cardSuit;
+        string clickedCardSuit = utils.clcikedCard.GetComponent<CardScript>().cardSuit;
+        if (selectedCardSuit.Equals("hearts") && clickedCardSuit.Equals("diamonds"))
+        {
+            
+            return true;
+        }
+        else if (selectedCardSuit.Equals("diamonds") && clickedCardSuit.Equals("hearts"))
+        {
+            return true;
+        }
+        else if (selectedCardSuit.Equals("spades") && clickedCardSuit.Equals("clubs"))
+        {
+            return true;
+        }
+        else if (selectedCardSuit.Equals("clubs") && clickedCardSuit.Equals("spades"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+      
+
+    }
+
+    //TODO:
+    //when there's like, 
     private int CountReactorCard()
     {
         //sum the values into totalSum, return
@@ -61,7 +104,7 @@ public class ReactorScript : MonoBehaviour
         while (counter < cardMax)
         {
             //this is going to need to be changed because card list is not an array
-            totalSum += cardList[counter].gameObject.GetComponent<CardScript>().cardVal;
+            totalSum += cardList[counter].gameObject.GetComponent<CardScript>().cardNum;
         }
 
         return totalSum;
