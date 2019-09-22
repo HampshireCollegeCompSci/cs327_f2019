2110971ab9ec8e1bc2e12f8b4eb18e098eec1d3a
Per Van Dyke <pav17@hampshire.edu>
2019-09-22 18:15:10 -0400

    foundation count now resets correctly

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -9,12 +9,15 @@ public class Config : MonoBehaviour
     //Variables go here
     public Stack<Move> moveLog = new Stack<Move>();
 
+    //foundations
     public GameObject foundation1;
     public GameObject foundation2;
     //public GameObject foundation3;
     //public GameObject foundation4;
 
     public GameObject[] foundationList;
+
+    int foundationCount = 0;
     void Awake()
     {
         if (config == null)
@@ -35,7 +38,7 @@ public class Config : MonoBehaviour
 
     private void Update()
     {
-        int foundationCount = 0;
+        //every frame, check to see if all foundations are empty.
         foreach (GameObject foundation in foundationList)
         {  
             if (foundation.GetComponent<FoundationScript>().cardList.Count == 0)
@@ -47,6 +50,10 @@ public class Config : MonoBehaviour
         if (foundationCount == foundationList.Length)
         {
             Application.Quit();
+        }
+        else
+        {
+            foundationCount = 0;
         }
     }
 }
