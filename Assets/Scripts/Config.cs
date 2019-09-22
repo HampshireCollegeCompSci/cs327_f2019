95582cda781c7ab6f9f527bb60321a88b11a33f9
Per Van Dyke <pav17@hampshire.edu>
2019-09-22 18:11:02 -0400

    Added framework to end game
    Currently configured for two foundations, will make it adjustable later. Application will quit when foundations are empty. (Will add UI stuff instead later)

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -9,6 +9,12 @@ public class Config : MonoBehaviour
     //Variables go here
     public Stack<Move> moveLog = new Stack<Move>();
 
+    public GameObject foundation1;
+    public GameObject foundation2;
+    //public GameObject foundation3;
+    //public GameObject foundation4;
+
+    public GameObject[] foundationList;
     void Awake()
     {
         if (config == null)
@@ -19,6 +25,28 @@ public class Config : MonoBehaviour
         else if (config != this)
         {
             Destroy(gameObject); //deletes copies of global which do not need to exist, so right version is used to get info from
+        }
+    }
+
+    private void Start()
+    {
+        foundationList = new GameObject[] { foundation1, foundation2/*, foundation3, foundation4*/ };
+    }
+
+    private void Update()
+    {
+        int foundationCount = 0;
+        foreach (GameObject foundation in foundationList)
+        {  
+            if (foundation.GetComponent<FoundationScript>().cardList.Count == 0)
+            {
+                foundationCount++;
+            }
+        }
+
+        if (foundationCount == foundationList.Length)
+        {
+            Application.Quit();
         }
     }
 }
