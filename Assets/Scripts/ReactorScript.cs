34dbede89d7996e81385d2da9641fcdd0729b3be
sophiaberger <sophia.alexandra.berger@gmail.com>
2019-09-22 11:23:54 -0400

    Changed reactor script

--- Assets/Scripts/ReactorScript.cs
+++ Assets/Scripts/ReactorScript.cs
@@ -30,11 +30,39 @@ public class ReactorScript : MonoBehaviour
 
     public void Clicked()
     {
+        if (CountReactorCard() < 18)
+        {
+            //TODO:
+            
+        }
+        else
+        {
+            //TODO: game over
+        }
         return;
+        //on click begin reactor function
+        //from utils
+        //if the count is under 18
+            //when you call Count Reactor Card
+            //check if a match
+            //if it's a match then do something? Unclear how to do
+            //else don't need to do anything
+        //else
+            //game over
+
     }
 
-    void CountReactorCard()
+    private int CountReactorCard()
     {
+        //sum the values into totalSum, return
+        int totalSum = 0;
+        counter = 0; //index for tracking position in cardlist
+        while (counter < cardMax)
+        {
+            //this is going to need to be changed because card list is not an array
+            totalSum += cardList[counter].gameObject.GetComponent<CardScript>().cardVal;
+        }
 
+        return totalSum;
     }
 }
