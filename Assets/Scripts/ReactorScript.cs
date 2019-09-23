06e520abe683dc88879c9b88b64024a7481ad616
sophiaberger <sophia.alexandra.berger@gmail.com>
2019-09-22 21:00:54 -0400

    Added comments to clarify my code
    Just added some comments so people can see what I'm doing/trying to do especially for debugging purposes since it is currently a pain in the ass to debug

--- Assets/Scripts/ReactorScript.cs
+++ Assets/Scripts/ReactorScript.cs
@@ -42,58 +42,64 @@ public class ReactorScript : MonoBehaviour
     public void Clicked()
     {
 
-        //this is the double clicking part I guess 
+        //this is the double clicking part in theory? Jona said it was how he
+        //compared for double clicking so if it's wrong my bad
+        //I did modify the index though because I thought it's the first
+        //if it's not the first index, I will change it back
         if (utils.GetComponent<UtilsScript>().clcikedCard == cardList[0])
         {
-            //utils.GetComponent<UtilsScript>().clcikedCard = null;
-            //this was Jona's I'm just leaving it in case I forget wtf I'm doing
+            //insert the clicked card into the list because in theory that's what we want?
+            //unless it's selected card, honestly not sure
             cardList.Insert(0, utils.GetComponent<UtilsScript>().clcikedCard);
+            //this was Jona's code initially, I'm just leaving it in case I forget wtf I'm doing
             //in theory make it null, e.g. deselecting? 
             utils.GetComponent<UtilsScript>().clcikedCard = null;
         }
-        else // select the top of the waste pile
+        else // if not double clicked, they aren't adding, whcih means they are trying to match
         {
-            //if the card numbers match
+            //if the card numbers match and the suits match (MatchSuit)
             if (utils.GetComponent<UtilsScript>().selectedCards[0].GetComponent<CardScript>().cardNum == utils.GetComponent<UtilsScript>().clcikedCard.GetComponent<CardScript>().cardNum && MatchSuit())
             {
                 utils.GetComponent<UtilsScript>().Match();
 
             }
         }
-       
-        
-        
+        //this is just the return call to end after having clicked
         return;
-        //if the cards match (check the attributes of the cards - you're only allowed to click top card
-        //not sure how to compare to other card yet
-        //then call match function
+        
         
 
     }
 
+    //this will need to be changed once I know what the actual string names are 
     public bool MatchSuit()
     {
         //just to make it cleaner because this utils.blah blah blah is yucky
         //basically a string of if/else cases for matching
         string selectedCardSuit = utils.GetComponent<UtilsScript>().selectedCards[0].GetComponent<CardScript>().cardSuit;
         string clickedCardSuit = utils.GetComponent<UtilsScript>().clcikedCard.GetComponent<CardScript>().cardSuit;
+        //hearts diamond combo #1
         if (selectedCardSuit.Equals("hearts") && clickedCardSuit.Equals("diamonds"))
         {
             
             return true;
         }
+        //hearts diamond combo #2
         else if (selectedCardSuit.Equals("diamonds") && clickedCardSuit.Equals("hearts"))
         {
             return true;
         }
+        //spades clubs combo #1
         else if (selectedCardSuit.Equals("spades") && clickedCardSuit.Equals("clubs"))
         {
             return true;
         }
+        //spades clubs combo #2
         else if (selectedCardSuit.Equals("clubs") && clickedCardSuit.Equals("spades"))
         {
             return true;
         }
+        //otherwise not a match 
         else
         {
             return false;
@@ -102,8 +108,10 @@ public class ReactorScript : MonoBehaviour
 
     }
 
-    //TODO:
-    //when there's like, 
+    //this is just meant to iterate through the list of cards in the stack
+    //sum the amounts of them, and then return whatever that sum is
+    //in order to be used in the update function
+    //basically just in case it goes over 18, in which case end game
     private int CountReactorCard()
     {
         //sum the values into totalSum, return
@@ -111,7 +119,8 @@ public class ReactorScript : MonoBehaviour
         counter = 0; //index for tracking position in cardlist
         while (counter < cardMax)
         {
-            //this is going to need to be changed because card list is not an array
+            //this is going to need to be changed because card list is not an array according to
+            //I think Ian? 
             totalSum += cardList[counter].gameObject.GetComponent<CardScript>().cardNum;
         }
 
