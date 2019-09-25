180df55e97e6318c2ed53e503c2e19b0b0c05d9c
Per Van Dyke <pav17@hampshire.edu>
2019-09-24 21:22:28 -0400

    Further simplification
    We actually don't need to loop at all, we can just clear the list.

--- Assets/Scripts/UtilsScript.cs
+++ Assets/Scripts/UtilsScript.cs
@@ -75,12 +75,8 @@ public class UtilsScript : MonoBehaviour
         else
         {
             selectedCards[0].GetComponent<CardScript>().container.SendMessage("Clicked", hit.collider.gameObject);
-            //we are no longer changing a list that we are also iterating over
-            int selectedCardsLength = selectedCards.Count;
-            for (int i = 0; i < selectedCardsLength; i++)
-            {
-                DeselectCard(selectedCards[0]);
-            }
+            //entire loop was unessisary
+            selectedCards.Clear();
         }
     }
 
