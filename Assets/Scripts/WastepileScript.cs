b76230802aa458d118d273ebfb8cc7a1c3048ccf
Per Van Dyke <pav17@hampshire.edu>
2019-09-25 18:03:53 -0400

    Mobile setup/wastepile
    Game is now set up for portrait mode re-wrote wastepile Process Action script, still WIP.

--- Assets/Scripts/WastepileScript.cs
+++ Assets/Scripts/WastepileScript.cs
@@ -42,13 +42,13 @@ public class WastepileScript : MonoBehaviour
         for (int i = cardList.Count - 1; i > -1; i--)
         {
             cardList[i].transform.parent = gameObject.transform;
-            cardList[i].transform.localPosition = new Vector3(0, -0.5f * counter, -0.1f * counter);
+            cardList[i].transform.localPosition = new Vector3(Config.config.foundationStackDensity * counter, 0, -0.1f * counter);
             counter++;
         }
     }
 
 
-    public void ProcessAction(GameObject input)
+    /*public void ProcessAction(GameObject input)
     {
         GameObject selectedCard = utils.selectedCards[0];
         // checking if utils.selectedCards only has the top card in the wastePile  
@@ -95,8 +95,38 @@ public class WastepileScript : MonoBehaviour
             }
         }
         return;
-    }
+    }*/
 
+    public void ProcessAction(GameObject input)
+    {
+        GameObject selectedCard = utils.selectedCards[0];
+        if (!input.CompareTag("Card"))
+        {
+            if ((input.CompareTag("Foundation") || input.CompareTag("Reactor")) && utils.selectedCards.Count != 0)
+            {
+                foreach (GameObject card in utils.selectedCards) //goes through and moves all selesctedCards to clicked location
+                {
+                    card.GetComponent<CardScript>().MoveCard(input);
+                }
+            }
+        }
+        else if (utils.IsMatch(input, utils.selectedCards[0]) && utils.selectedCards.Count == 1) //check if selectedCards and the input card match and that selesctedCards is only one card
+        {
+            utils.Match(input, utils.selectedCards[0]); //removes the two matched cards
+        }
+        else if ((utils.selectedCards[0].GetComponent<CardScript>().cardNum + 1) == input.GetComponent<CardScript>().cardNum && input.GetComponent<CardScript>().container.CompareTag("Foundation"))
+        {
+            foreach (GameObject card in utils.selectedCards) //goes through and moves all selesctedCards to clicked location
+            {
+                card.GetComponent<CardScript>().MoveCard(input.GetComponent<CardScript>().container);
+            }
+        }
+        else if (input.GetComponent<CardScript>().container.CompareTag("Reactor") && utils.IsSameSuit(input, utils.selectedCards[0]) && utils.selectedCards.Count == 1)
+        {
+            utils.selectedCards[0].GetComponent<CardScript>().MoveCard(input.GetComponent<CardScript>().container);
+        }
+        return;
+    }
     public List<GameObject> GetCardList()
     {
         return cardList;
