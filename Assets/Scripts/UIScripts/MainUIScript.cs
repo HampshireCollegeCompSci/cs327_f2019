1b216d5c9428ddebb7ec5cfaf96985c3a9c46818
Max <magicmaxmark@gmail.com>
2019-10-05 12:56:56 -0400

    Made a basic UI with a menu

---
+++ Assets/Scripts/UIScripts/MainUIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUIScript : MonoBehaviour
+{
+	public void Restart()
+	{
+		Application.LoadLevel(0);//resets the level
+		return;
+	}
+
+	public void SetGameMenu(GameObject gameMenu)
+	{
+        Debug.Log(gameMenu.activeSelf);
+
+        if (gameMenu.activeSelf)
+        {
+            gameMenu.SetActive(false);
+        }
+
+        else if (!gameMenu.activeSelf)
+        {
+            gameMenu.SetActive(true);
+        }
+	}
+}
---
+++ Assets/Scripts/UIScripts/MenuUIScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUIScript : MonoBehaviour
+{
+    public void NewGame(int difficulty)
+    {
+        Application.LoadLevel(0);//restarts the level there will be more added to this method later but for now we don't have dificulty
+        return;
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(0);//resets the level
+        return;
+    }
+
+    public void CloseGameMenu(GameObject gameMenu)
+    {
+        gameMenu.SetActive(false);
+    }
+}
