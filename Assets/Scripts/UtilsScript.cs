075f489d3655667007e049db37cb3e356dcf4692
Per Van Dyke <pav17@hampshire.edu>
2019-10-11 00:32:50 -0400

    Fixed NullPointerExeptions in main menu

--- Assets/Scripts/UtilsScript.cs
+++ Assets/Scripts/UtilsScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UtilsScript : MonoBehaviour
 {
@@ -32,7 +33,7 @@ public class UtilsScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && SceneManager.GetActiveScene().buildIndex != 0)
         {
             Click();
         }
