7120d2f0bdedbdaf5d0dfa7837d8fb45f68622b2
magicmaxmark <magicmaxmark@gmail.com>
2019-10-23 22:32:03 -0400

    Revert "added a find to the soundcontroller in restart"
    This reverts commit c863edf7de7b36c830150d611c4175a73568fb4a.

--- Assets/Scripts/UtilsScript.cs
+++ Assets/Scripts/UtilsScript.cs
@@ -10,7 +10,7 @@ public class UtilsScript : MonoBehaviour
     private List<GameObject> selectedCardsCopy = new List<GameObject>();
     public GameObject matchedPile;
     public GameObject gameUI;
-    public AudioSource soundController;
+    public SoundController soundController;
     public int indexCounter;
     public RaycastHit2D hit;
     private bool dragOn;
@@ -24,7 +24,6 @@ public class UtilsScript : MonoBehaviour
 
     public void SetCards()
     {
-        soundController = GameObject.Find("Sound").GetComponent<AudioSource>();
         matchedPile = GameObject.Find("MatchedPile");
         gameUI = GameObject.Find("GameUI");
     }
@@ -52,6 +51,7 @@ public class UtilsScript : MonoBehaviour
                 Click();
                 if (selectedCards.Count > 0)
                 {
+                    soundController.CardPressSound();
                     dragOn = true;
                 }
 
@@ -82,7 +82,7 @@ public class UtilsScript : MonoBehaviour
 
             if (dragOn == true && SceneManager.GetActiveScene().buildIndex == 2)
             {
-                CardPressSound();
+
                 ClickAndDrag(selectedCardsCopy);
             }
 
@@ -233,6 +233,7 @@ public class UtilsScript : MonoBehaviour
 
     public void Match(GameObject card1, GameObject card2)
     {
+        soundController.CardMatchSound();
         card1.GetComponent<CardScript>().MoveCard(matchedPile);
         card2.GetComponent<CardScript>().MoveCard(matchedPile);
 
@@ -364,11 +365,7 @@ public class UtilsScript : MonoBehaviour
         }
     }
 
-    private void CardPressSound()
-    {
-        AudioClip pressSound = Resources.Load<AudioClip>("Audio/token_select");
-        soundController.clip = pressSound;
-        soundController.Play();
-    }
+
+
 
 }
