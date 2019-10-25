acb4d0f0749ff30e4bd92cf8826912b4a4b2cb10
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2019-10-25 15:06:24 -0400

    implemented lose music and token reveal sound

---
+++ Assets/Scripts/Sound/MusicController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource soundController;
+
+    public void LoseMusic()
+    {
+        AudioClip sound = Resources.Load<AudioClip>("Audio/music_lose");
+        soundController.clip = sound;
+        soundController.Play();
+    }
+
+    public void WinMusic()
+    {
+        AudioClip sound = Resources.Load<AudioClip>("Audio/music_win");
+        soundController.clip = sound;
+        soundController.Play();
+    }
+
+    public void MainMenuMusic()
+    {
+        AudioClip sound = Resources.Load<AudioClip>("Audio/music_menu");
+        soundController.clip = sound;
+        soundController.Play();
+    }
+
+    public void GameMusic()
+    {
+        soundController.clip = null;
+        soundController.Play();
+    }
+}
