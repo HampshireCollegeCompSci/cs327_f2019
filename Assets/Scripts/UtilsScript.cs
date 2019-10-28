57acc90d6704f3afea5a2a416bea79794a3fba48
magicmaxmark <magicmaxmark@gmail.com>
2019-10-28 11:36:24 -0400

    Fixed Ians stacking bug

--- Assets/Scripts/UtilsScript.cs
+++ Assets/Scripts/UtilsScript.cs
@@ -367,11 +367,17 @@ public class UtilsScript : MonoBehaviour
 
         for(int i = 0; i < cards.Count; i++)
         {
-            cards[i].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y + cards[i].transform.position.y + i * Config.config.draggedTokenOffset, i));
+            if (i == 0)
+            {
+                cards[i].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y + i * Config.config.draggedTokenOffset, 1));
+            }
+
+            else
+            {
+                cards[i].transform.position =
+                    new Vector3(cards[i - 1].transform.position.x, cards[i - 1].transform.position.y + Config.config.draggedTokenOffset, cards[i - 1].transform.position.z - 0.05f);
+            }
         }
     }
-
-
-
-
 }
