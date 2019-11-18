948e64946db1720d6f22fdfeac20b8d49831b89c
Max <magicmaxmark@gmail.com>
2019-11-18 14:26:39 -0500

    Improved Credits Scene

---
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/NotAtAllSuspiciousFile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotAtAllSuspiciousFile : MonoBehaviour
+{
+    public void Max()
+    {
+        Config.config.prettyColors = !Config.config.prettyColors;
+    }
+}
