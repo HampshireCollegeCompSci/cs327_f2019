27a069d97eac5d5afc910febd7511f4cc37894b9
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2019-11-27 12:45:13 -0500

    added and claened up meta

---
+++ Assets/Scripts/UIScripts/SpaceBabyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceBabyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Animator>().Play("IdleAnim");
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent<Animator>().Play("HappyAnim");
+        StartCoroutine("BabyAnimTrans");
+    }
+
+    IEnumerator BabyAnimTrans()
+    {
+
+        for (float ft = 0.5f; ft >= 0; ft -= 0.1f)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        gameObject.GetComponent<Animator>().Play("IdleAnim");
+
+    }
+
+}
