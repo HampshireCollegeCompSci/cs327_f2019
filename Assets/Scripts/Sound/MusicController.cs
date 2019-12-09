5ca17b941bfdc6c2cf171902f5a3281c4d5e81c4
JonaLaw <33699564+JonaLaw@users.noreply.github.com>
2019-12-09 14:57:36 -0500

    Better Music Logic
    Doesn't load music files as much

--- Assets/Scripts/Sound/MusicController.cs
+++ Assets/Scripts/Sound/MusicController.cs
@@ -7,92 +7,79 @@ public class MusicController : MonoBehaviour
     public AudioSource soundTrack1;
     public AudioSource soundTrack2;
     private bool playing1 = false;
+    private byte playingTrack = 0;
 
     public void LoadGap()
     {
         soundTrack1.Stop();
         soundTrack2.Stop();
     }
+    public void MainMenuMusic()
+    {
+        if (playingTrack == 1)
+        {
+            return;
+        }
+
+        playingTrack = 1;
+        Transition(Resources.Load<AudioClip>("Audio/music_menu"));
+    }
+
+    public void GameMusic(bool startNew = false)
+    {
+        if (playingTrack == 2)
+        {
+            return;
+        }
+
+        playingTrack = 2;
+        Transition(Resources.Load<AudioClip>("Audio/music_main_theme"));
+    }
+
+    public void AlertMusic()
+    {
+        if (playingTrack == 3)
+        {
+            return;
+        }
+
+        playingTrack = 3;
+        Transition(Resources.Load<AudioClip>("Audio/music_transition"));
+    }
 
     public void LoseMusic()
     {
+        if (playingTrack == 4)
+        {
+            return;
+        }
+
+        playingTrack = 4;
         Transition(Resources.Load<AudioClip>("Audio/music_lose"));
     }
 
     public void WinMusic()
     {
+        if (playingTrack == 5)
+        {
+            return;
+        }
+
+        playingTrack = 5;
         Transition(Resources.Load<AudioClip>("Audio/music_win"));
-    }
-
-    public void MainMenuMusic()
-    {
-        Transition(Resources.Load<AudioClip>("Audio/music_menu"));
-    }
-
-    public void AlertMusic()
-    {
-        Transition(Resources.Load<AudioClip>("Audio/music_transition"));
-    }
-
-    public void GameMusic(bool startNew = false)
-    {
-        if (startNew)
-        {
-            AudioClip gm = Resources.Load<AudioClip>("Audio/music_main_theme");
-            if (playing1 && soundTrack1.clip == gm)
-            {
-                soundTrack1.volume = 1;
-            }
-            else if (!playing1 && soundTrack2.clip == gm)
-            {
-                soundTrack2.volume = 1;
-            }
-            else
-            {
-                Transition(Resources.Load<AudioClip>("Audio/music_main_theme"));
-                return;
-                soundTrack1.Stop();
-                soundTrack2.Stop();
-                soundTrack1.clip = gm;
-                soundTrack1.volume = 1;
-                soundTrack1.Play();
-                playing1 = true;
-            }
-        }
-        else
-        {
-            Transition(Resources.Load<AudioClip>("Audio/music_main_theme"));
-        }
     }
 
     public void Transition(AudioClip newTrack)
     {
+        StopAllCoroutines();
         if (playing1)
         {
-            if (soundTrack1.clip == newTrack)
-            {
-                if (soundTrack1.volume != 1)
-                {
-                    StartCoroutine(FadeIn(soundTrack1, 1f, true));
-                }
-                return;
-            }
-
             soundTrack2.clip = newTrack;
             StartCoroutine(FadeOut(soundTrack1, 1f));
             StartCoroutine(FadeIn(soundTrack2, 1f));
         }
         else
         {
-            if (soundTrack2.clip == newTrack)
-            {
-                if (soundTrack2.volume != 1)
-                {
-                    StartCoroutine(FadeIn(soundTrack2, 1f, true));
-                }
-                return;
-            }
-
             soundTrack1.clip = newTrack;
             StartCoroutine(FadeOut(soundTrack2, 1f));
             StartCoroutine(FadeIn(soundTrack1, 1f));
@@ -103,6 +90,11 @@ public class MusicController : MonoBehaviour
     // https://medium.com/@wyattferguson/how-to-fade-out-in-audio-in-unity-8fce422ab1a8
     public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
     {
+        if (!audioSource.isPlaying)
+        {
+            yield break;
+        }
+
         float startVolume = audioSource.volume;
         while (audioSource.volume > 0)
         {
@@ -111,13 +103,10 @@ public class MusicController : MonoBehaviour
         }
         audioSource.Stop();
     }
-    public static IEnumerator FadeIn(AudioSource audioSource, float FadeTime, bool keepOnGoing = false)
+    public static IEnumerator FadeIn(AudioSource audioSource, float FadeTime)
     {
+        audioSource.volume = 0;
         audioSource.Play();
-        if (!keepOnGoing)
-        {
-            audioSource.volume = 0f;
-        }
         while (audioSource.volume < 1)
         {
             audioSource.volume += Time.deltaTime / FadeTime;
