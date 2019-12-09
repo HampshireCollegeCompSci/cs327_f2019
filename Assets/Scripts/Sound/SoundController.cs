5b050876796c7d05781ed6113aedb113817c899d
JonaLaw <jonaklawrence@gmail.com>
2019-12-09 18:36:09 -0500

    Changed most sounds to PlayOneShot

--- Assets/Scripts/Sound/SoundController.cs
+++ Assets/Scripts/Sound/SoundController.cs
@@ -8,136 +8,114 @@ public class SoundController : MonoBehaviour
 
     public void ButtonPressSound()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/button_press");
-        soundController.clip = sound;
+        // doesn't like PlayOneShot
+        soundController.clip = Resources.Load<AudioClip>("Audio/button_press");
         soundController.Play();
     }
     public void ButtonPressSound2()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/token_reveal");
-        // volunme changes doesn't work
-        soundController.volume = 0.5f;
-        soundController.clip = sound;
-        soundController.Play();
-        soundController.volume = 1;
+        soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_reveal"), 0.5f);
     }
 
     public void CardPressSound()
     {
-        int randomNo = Random.Range(0, 2);
-        AudioClip sound = Resources.Load<AudioClip>("Audio/token_select");
-        if (randomNo == 1)
-            sound = Resources.Load<AudioClip>("Audio/token_select_B");
-        else if (randomNo == 2)
-            sound = Resources.Load<AudioClip>("Audio/token_select_C");
-
-        soundController.clip = sound;
-        soundController.Play();
+        int randomNo = Random.Range(0, 3);
+        Debug.Log(randomNo);
+        if (randomNo == 0)
+        {
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_select"));
+        }
+        else if (randomNo == 1)
+        {
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_select_B"));
+        }
+        else
+        {
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_select_C"));
+        }
     }
 
     public void CardToReactorSound()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/token_placed_in_reactor");
-        soundController.clip = sound;
-        soundController.Play();
+        soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_placed_in_reactor"));
     }
 
     public void CardStackSound()
     {
-        int randomNo = Random.Range(0, 3);
-        AudioClip sound = Resources.Load<AudioClip>("Audio/token_stack");
-        if (randomNo == 1)
-            sound = Resources.Load<AudioClip>("Audio/token_stack_B");
-        else if (randomNo == 2)
-            sound = Resources.Load<AudioClip>("Audio/token_stack_C");
-        else if (randomNo == 3)
-            sound = Resources.Load<AudioClip>("Audio/token_stack_D");
 
-        soundController.clip = sound;
-        soundController.Play();
+        int randomNo = Random.Range(0, 4);
+        if (randomNo == 0)
+        {
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_stack"));
+        }
+        else if (randomNo == 1)
+        {
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_stack_B"));
+        }
+        else if (randomNo == 2)
+        {
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_stack_C"));
+        }
+        else
+        {
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_stack_D"));
+        }
     }
 
     public void CardRevealSound()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/token_reveal");
-        soundController.clip = sound;
-        soundController.Play();
+        soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/token_reveal"));
     }
 
     public void CardMatchSound()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/card_match_c");
-        soundController.clip = sound;
-        soundController.Play();
+        soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/card_match_c"));
     }
 
     public void DeckDeal()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/deck_conveyer_belt_deal");
-        soundController.Stop();
-        soundController.volume = 1;
-        soundController.clip = sound;
-        soundController.Play();
+        soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/deck_conveyer_belt_deal"), 0.3f);
     }
 
     public void DeckReshuffle()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/deck_conveyer_belt_reshuffle");
-        soundController.Stop();
-        soundController.volume = 0.3f;
-        soundController.clip = sound;
-        soundController.Play();
+        soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/deck_conveyer_belt_reshuffle"), 0.3f);
     }
 
     public void PauseMenuButtonSound()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/pause_menu");
-        soundController.clip = sound;
+        soundController.Stop();
+        soundController.clip = Resources.Load<AudioClip>("Audio/pause_menu");
         soundController.Play();
     }
 
     public void ReactorExplodeSound()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/reactor_explosion");
-        soundController.clip = sound;
-        soundController.Play();
+        soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/reactor_explosion"));
     }
 
     public void WinSound()
     {
-        AudioClip sound = Resources.Load<AudioClip>("Audio/sound_win");
-        soundController.clip = sound;
+        soundController.clip = Resources.Load<AudioClip>("Audio/sound_win"); ;
         soundController.Play();
     }
     public void FoodMatch(string suit)
     {
         if(suit == "hearts")
         {
-            AudioClip sound = Resources.Load<AudioClip>("Audio/food_mushroom");
-            print(sound);
-            soundController.clip = sound;
-            soundController.Play();
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/food_mushroom"));
         }
         if (suit == "diamonds")
         {
-            AudioClip sound = Resources.Load<AudioClip>("Audio/food_bug");
-            soundController.clip = sound;
-            print(sound);
-            soundController.Play();
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/food_bug"));
         }
         if (suit == "spades")
         {
-            AudioClip sound = Resources.Load<AudioClip>("Audio/food_rock");
-            print(sound);
-            soundController.clip = sound;
-            soundController.Play();
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/food_rock"));
         }
         if (suit == "clubs")
         {
-            AudioClip sound = Resources.Load<AudioClip>("Audio/food_fruit");
-            print(sound);
-            soundController.clip = sound;
-            soundController.Play();
+            soundController.PlayOneShot(Resources.Load<AudioClip>("Audio/food_fruit"));
         }
 
 
