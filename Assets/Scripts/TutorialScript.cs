dcd701d4a785a91f49ec0703fd91478a91f901a0
Per Van Dyke <pav17@hampshire.edu>
2020-02-07 15:38:26 -0500

    fleshing out tutorial system

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -4,7 +4,44 @@ using UnityEngine;
 
 public class TutorialScript : MonoBehaviour
 {
-    
+    private List<string> commandList;
+    private Queue<string[]> splitCommandList;
+    void Start()
+    {
+        commandList = CreateFromJSON();
+        CommandReader(commandList);
+    }
+
+    void CommandReader(List<string> commandList)
+    {
+        foreach (string command in commandList)
+        {
+            string[] splitCommand = command.Split(_);
+            splitCommandList.Enqueue(splitCommand);
+        }
+    }
+
+    void CommandInterpreter()
+    {
+        string[] command = splitCommandList.Dequeue();
+        if (command[0] == "LoadSave")
+        {
+            LoadSave(command[1]);
+        }
+        else if (command[0] == "ShowMask")
+        {
+            ShowMask(command[1]);
+        }
+        else if (command[0] == "ShowText")
+        {
+            ShowText(command[1], command[2]);
+        }
+        else if (command[0] == "WaitForTouch")
+        {
+            WaitForTouch();
+        }
+    }
+
     //Tutorial Commands
     public void WaitForTouch()
     {
@@ -23,5 +60,12 @@ public class TutorialScript : MonoBehaviour
     public void ShowText(string text, string region)
     {
 
+    }
+
+    private static List<string> CreateFromJSON()
+    {
+        var jsonTextFile = Resources.Load<TextAsset>("Tutorial/TutorialCommandList");
+        TutorialCommands commandFile = JsonUtility.FromJson<TutorialCommands>(jsonTextFile.ToString());
+        return commandFile.commands;
     }
 }
