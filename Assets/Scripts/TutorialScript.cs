9e0921b5cd4196cc281c553381ba42319c013c67
Per Van Dyke <pav17@hampshire.edu>
2020-02-07 15:10:38 -0500

    Set up infrastructure for tutorial system

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    
+    //Tutorial Commands
+    public void WaitForTouch()
+    {
+
+    }
+
+    public void LoadSave(string fileName)
+    {
+
+    }
+    public void ShowMask(string fileName)
+    {
+
+    }
+
+    public void ShowText(string text, string region)
+    {
+
+    }
+}
