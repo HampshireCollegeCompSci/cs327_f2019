3ed4fc22e1b05bd4b3c234c6ddcbf09217598f76
Per Van Dyke <pav17@hampshire.edu>
2020-02-14 17:14:51 -0500

    Continue now hides if no save

---
+++ Assets/Scripts/UIScripts/ContinueHider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ContinueHider : MonoBehaviour
+{
+    private void Awake()
+    {
+        if(!File.Exists(Application.persistentDataPath + "/testState.json"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
