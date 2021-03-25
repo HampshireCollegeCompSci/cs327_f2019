eccf57c6ef08f267f71ab3494d774ef9a99b130b
JonaLaw <jonaklawrence@gmail.com>
2021-03-25 18:24:17 -0400

    Removed obsolete code
    The reactor score text was pinned to the scene some time ago.

--- Assets/Scripts/UIScripts/ReactorScoreSetScript.cs
+++ Assets/Scripts/UIScripts/ReactorScoreSetScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class ReactorScoreSetScript : MonoBehaviour
@@ -8,7 +6,6 @@ public class ReactorScoreSetScript : MonoBehaviour
     private void Start()
     {
         SetReactorScore();
-        SetPositions();
     }
 
     public GameObject reactor0;
@@ -29,20 +26,6 @@ public class ReactorScoreSetScript : MonoBehaviour
         reactorScore1.text = reactor1.GetComponent<ReactorScript>().CountReactorCard() + "/" + Config.config.maxReactorVal;
         reactorScore2.text = reactor2.GetComponent<ReactorScript>().CountReactorCard() + "/" + Config.config.maxReactorVal;
         reactorScore3.text = reactor3.GetComponent<ReactorScript>().CountReactorCard() + "/" + Config.config.maxReactorVal;
-    }
-
-    public void SetPositions()
-    {
-        return;
-
-        if (reactorScore0 != null && reactorScore1 != null && reactorScore2 != null && reactorScore3 != null
-            && reactor0 != null && reactor1 != null && reactor2 != null && reactor3 != null)
-        {
-            reactorScore0.transform.position = reactor0.transform.position - new Vector3(-0.05f, 1.45f, 0);
-            reactorScore1.transform.position = reactor1.transform.position - new Vector3(0, 1.45f, 0);
-            reactorScore2.transform.position = reactor2.transform.position - new Vector3(0, 1.45f, 0);
-            reactorScore3.transform.position = reactor3.transform.position - new Vector3(0, 1.45f, 0);
-        }
     }
 
     public void ChangeTextColor(GameObject reactor, bool on)
