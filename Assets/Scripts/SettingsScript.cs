05c18913b8c3162ffe87481f29c591a002089067
JonaLaw <jonaklawrence@gmail.com>
2021-05-16 16:01:39 -0400

    Added work in progress settings scene

---
+++ Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScript : MonoBehaviour
+{
+    public GameObject effectsVolumeText;
+    public GameObject musicVolumeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("effectsVolume"))
+        {
+
+        }
+    }
+
+    public void EffectsVolumeChange(float update)
+    {
+        PlayerPrefs.SetFloat("effectsVolume", update);
+    }
+
+    public void MusicVolumeChange(float update)
+    {
+        PlayerPrefs.SetFloat("musicVolume", update);
+    }
+
+    public void VibrationOnToggle(bool update)
+    {
+        PlayerPrefs.SetString("vibrationEnabled", update.ToString());
+    }
+}
