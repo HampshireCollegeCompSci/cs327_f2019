1ba42a3aeec1ca129663007402f567935f404bb8
JonaLaw <33699564+JonaLaw@users.noreply.github.com>
2023-03-14 14:57:34 -0700

    Added Camera Controller
    When new scenes are loaded, their camera is enabled whilst the previous camera is disabled. When scenes are unloaded, their camera is disabled whilst the previous camera is enabled.
    This solves an issue where the pause scene was not being shown after backing out of the settings scene.

---
+++ Assets/Scripts/UIScripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private void Start()
+    {
+        Config.Instance.AddCamera(this.gameObject.GetComponent<Camera>());
+    }
+
+    private void OnDestroy()
+    {
+        Config.Instance.RemoveCamera(this.gameObject.GetComponent<Camera>());
+    }
+}
